ff85563ff478bf1e8d5ef1578137a33f21af4e44
Mauri Mustonen <mauri_mustonen@hotmail.com>
2021-02-22 08:15:58 +0200

    vplan support for widening call and select instructions

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -8917,7 +8917,7 @@ VPlanPtr LoopVectorizationPlanner::buildVPlan(VFRange &Range) {
 
   SmallPtrSet<Instruction *, 1> DeadInstructions;
   VPlanTransforms::VPInstructionsToVPRecipes(
-      OrigLoop, Plan, Legal->getInductionVars(), DeadInstructions);
+      PSE, OrigLoop, Plan, Legal->getInductionVars(), DeadInstructions);
   return Plan;
 }
 
